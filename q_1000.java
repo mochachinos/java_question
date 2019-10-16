21b78338aa90a9448a72830b54abec154bebe54a
lemonperfume <lemonlemonjuicy@gmail.com>
2019-10-16 23:41:18 +0900

    q_1000

---
+++ q_1000.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*
+문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+입력 : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+출력 : 첫째 줄에 A+B를 출력한다.
+*/
+public class q_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a + b);
+    }
+}
